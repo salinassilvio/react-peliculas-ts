8465756c112961bc66255f8d91609a952e95c117
Silvio Salinas <ssalinas.silvio@gmail.com>
2021-11-28 21:20:51 -0600

    feat(listar genero): listar generos
    Realizar listado de generos por medio de map mostrandolo en table

--- src/components/generos/IndiceGeneros.tsx
+++ src/components/generos/IndiceGeneros.tsx
@@ -1,15 +1,20 @@
 import axios, { AxiosResponse } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { generoDTO } from "../../models/generos.model";
+import { generoCreacionDTO, generoDTO } from "../../models/generos.model";
+import Button from "../utils/Button";
 import { urlGeneros } from "../utils/endpoint";
+import ListadoGenerico from "../utils/ListadoGenerico";
 
 export default  function IndiceGeneros(){
+
+    const [generos,setGeneros] = useState<generoDTO[]>();
 
     useEffect(() => {
        axios.get(urlGeneros)
        .then((respuesta: AxiosResponse<generoDTO[]>) => {
            console.log(respuesta.data);
+           setGeneros(respuesta.data);
        })
     }, [])
 
@@ -17,6 +22,32 @@ export default  function IndiceGeneros(){
         <>
             <h3>Generos</h3>
             <Link className="btn btn-primary" to="generos/crear">Crear Genero</Link>
+
+            <ListadoGenerico listado={generos}>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Nombre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {generos?.map(genero => 
+                            <tr key={genero.id}>
+                                <td>
+                                    <Link className="btn btn-success" to={`/genero/${genero.id}`}>
+                                        Editar
+                                    </Link>
+                                    <Button className="btn btn-danger">Borrar</Button>
+                                </td>
+                                <td>
+                                    {genero.nombre}
+                                </td>
+                            </tr>
+                            )}
+                    </tbody>
+                </table>
+            </ListadoGenerico>
         </>
     )
 }
