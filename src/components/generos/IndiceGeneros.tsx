5f805dae096c01b72e077a8c522eb952ea124ce7
Silvio Salinas <ssalinas.silvio@gmail.com>
2021-12-11 00:39:13 -0600

    the pagination component is finished

--- src/components/generos/IndiceGeneros.tsx
+++ src/components/generos/IndiceGeneros.tsx
@@ -5,49 +5,86 @@ import { generoCreacionDTO, generoDTO } from "../../models/generos.model";
 import Button from "../utils/Button";
 import { urlGeneros } from "../utils/endpoint";
 import ListadoGenerico from "../utils/ListadoGenerico";
+import Paginacion from "../utils/Paginacion";
 
-export default  function IndiceGeneros(){
+export default function IndiceGeneros() {
+  const [generos, setGeneros] = useState<generoDTO[]>();
+  const [totalDePaginas, setTotalDePaginas] = useState(0);
+  const [recordsPorPagina, setRecordsPorPagina] = useState(10);
+  const [pagina, setPagina] = useState(1);
 
-    const [generos,setGeneros] = useState<generoDTO[]>();
+  useEffect(() => {
+    axios
+      .get(urlGeneros, {
+        params: { pagina, recordsPorPagina },
+      })
+      .then((respuesta: AxiosResponse<generoDTO[]>) => {
+        console.log(respuesta.headers["cantidadtotalregistro"]);
+        const totalDeRegistros = parseInt(
+          respuesta.headers["cantidadtotalregistro"],
+          10
+        );
+        console.error(["AQUI", totalDeRegistros]);
 
-    useEffect(() => {
-       axios.get(urlGeneros)
-       .then((respuesta: AxiosResponse<generoDTO[]>) => {
-           console.log(respuesta.data);
-           setGeneros(respuesta.data);
-       })
-    }, [])
+        setTotalDePaginas(Math.ceil(totalDeRegistros / recordsPorPagina));
+        console.log(respuesta.data);
+        setGeneros(respuesta.data);
+      });
+  }, [pagina, recordsPorPagina]);
 
-    return (
-        <>
-            <h3>Generos</h3>
-            <Link className="btn btn-primary" to="generos/crear">Crear Genero</Link>
+  return (
+    <>
+      <h3>Géneros</h3>
+      <Link className="btn btn-primary" to="generos/crear">
+        Crear Genero
+      </Link>
 
-            <ListadoGenerico listado={generos}>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Nombre</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {generos?.map(genero => 
-                            <tr key={genero.id}>
-                                <td>
-                                    <Link className="btn btn-success" to={`/genero/${genero.id}`}>
-                                        Editar
-                                    </Link>
-                                    <Button className="btn btn-danger">Borrar</Button>
-                                </td>
-                                <td>
-                                    {genero.nombre}
-                                </td>
-                            </tr>
-                            )}
-                    </tbody>
-                </table>
-            </ListadoGenerico>
-        </>
-    )
+      <div className="form-group" style={{ width: "150px" }}>
+        <label>Registros por página:</label>
+        <select
+          className="form-control"
+          defaultValue={10}
+          onChange={(e) => {
+            setPagina(1);
+            setRecordsPorPagina(parseInt(e.currentTarget.value, 10));
+          }}
+        >
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={25}>25</option>
+          <option value={50}>50</option>
+        </select>
+      </div>
+
+      <Paginacion
+        cantidadTotalDePaginas={totalDePaginas}
+        paginaActual={pagina}
+        onChange={(nuevaPagina) => setPagina(nuevaPagina)}
+      ></Paginacion>
+
+      <ListadoGenerico listado={generos}>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Nombre</th>
+            </tr>
+          </thead>
+          <tbody>
+            {generos?.map((genero) => (
+              <tr key={genero.id}>
+                <td>
+                  <Link className="btn btn-success" to={`/genero/${genero.id}`}>
+                    Editar
+                  </Link>
+                  <Button className="btn btn-danger">Borrar</Button>
+                </td>
+                <td>{genero.nombre}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </ListadoGenerico>
+    </>
+  );
 }
