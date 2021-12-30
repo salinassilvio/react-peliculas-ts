ff20ba189cd9fc15bf92a2faf59470c7db91c8f7
Silvio Salinas <ssalinas.silvio@gmail.com>
2021-12-29 22:26:03 -0600

    consume endpoint and create components Actores

--- src/components/actores/CrearActores.tsx
+++ src/components/actores/CrearActores.tsx
@@ -1,13 +1,39 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { actorCreacionDTO } from "../../models/actores.model";
+import { urlActores } from "../utils/endpoint";
+import { convertirActorAFormData } from "../utils/formDataUtils";
+import MostrarErrores from "../utils/MostrarErrores";
 import FormularioActores from "./FormularioActores";
 
-export default function CrearActores(){
-    return(
-        <>
-        <>Crear Actores</>
-        <FormularioActores
-            modelo={{nombre:'',fechaNacimiento:undefined}}
-            onSubmit={valores => console.log(valores)}
-        ></FormularioActores>
-        </>
-    )
+export default function CrearActores() {
+  const [errores, setErrores] = useState<string[]>([]);
+  const history = useHistory();
+
+  async function crear(actor: actorCreacionDTO) {
+    try {
+      const formData = convertirActorAFormData(actor);
+      await axios({
+        method: "POST",
+        url: urlActores,
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      history.push("/actores");
+    } catch (error) {
+      setErrores(error.response.data);
+    }
+  }
+
+  return (
+    <>
+      <>Crear Actores</>
+      <MostrarErrores errores={errores}></MostrarErrores>
+      <FormularioActores
+        modelo={{ nombre: "", fechaNacimiento: undefined }}
+        onSubmit={async (valores) => await crear(valores)}
+      ></FormularioActores>
+    </>
+  );
 }
---
+++ src/components/utils/formDataUtils.ts
@@ -0,0 +1,33 @@
+import { actorCreacionDTO } from "./../../models/actores.model.d";
+
+export function convertirActorAFormData(actor: actorCreacionDTO): FormData {
+  const formData = new FormData();
+
+  formData.append("nombre", actor.nombre);
+  if (actor.biografia) {
+    formData.append("biografia", actor.biografia);
+  }
+  if (actor.fechaNacimiento) {
+    formData.append("fechaNacimiento", formatearFecha(actor.fechaNacimiento));
+  }
+
+  if (actor.foto) {
+    formData.append("foto", actor.foto);
+  }
+
+  return formData;
+}
+
+function formatearFecha(date: Date) {
+  date = new Date();
+  const formato = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  const [{ value: month }, , { value: day }, , { value: year }] =
+    formato.formatToParts(date);
+
+  return `${year}-${month}-${day}`;
+}
